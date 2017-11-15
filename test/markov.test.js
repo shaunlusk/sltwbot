de5eabed0b4b48fc4c251f9e24db7c945007424f
Shaun Lusk <shaun.lusk@gmail.com>
2017-11-14 22:21:19 -0600

    sentencizing tests, start tokenizing work

--- test/markov.test.js
+++ test/markov.test.js
@@ -11,8 +11,73 @@ describe('#sentencize', function() {
     ];
     var sentenceGroup = expected.join(' ');
     var result = markov.sentencize(sentenceGroup);
+    assert.isArray(result);
     assert.lengthOf(result, expected.length, 'should have returned ' + expected.length + ' sentences');
-    assert.equal(result, expected, 'should have returned sentences');
+    assert.deepEqual(result, expected, 'should have returned sentences');
+    done();
+  });
+  it('should return empty array', function(done) {
+    var expected = [];
+    var sentenceGroup = ' .';
+    var result = markov.sentencize(sentenceGroup);
+    assert.isArray(result);
+    assert.lengthOf(result, expected.length, 'should have returned ' + expected.length + ' sentences');
+    assert.deepEqual(result, expected, 'should have returned no sentences');
+    done();
+  });
+  it('should return array of sentences, include partials', function(done) {
+    var expected = [
+      'This is a test!',
+      'This is only a test.',
+      'Is this a question?',
+      'Check'
+    ];
+    var sentenceGroup = expected.join(' ');
+    var result = markov.sentencize(sentenceGroup);
+    assert.isArray(result);
+    assert.lengthOf(result, expected.length, 'should have returned ' + expected.length + ' sentences');
+    assert.deepEqual(result, expected, 'should have returned sentences');
+    done();
+  });
+  it('should should not return junk sentences', function(done) {
+    var expected = [
+      'This is a test!',
+      'This is only a test.',
+      'Is this a question?',
+      ' .. '
+    ];
+    var sentenceGroup = expected.join(' ');
+    var result = markov.sentencize(sentenceGroup);
+    assert.isArray(result);
+    assert.lengthOf(result, expected.length - 1, 'should have returned ' + expected.length + ' sentences');
+    for (var i = 0; i < expected.length - 1; i++) {
+      assert.equal(result[i], expected[i], 'should have returned sentences');
+    }
+    done();
+  });
+  it('should should not return junk sentences', function(done) {
+    var expected = [
+      'This is only a test.',
+      ' .. ',
+      'Is this a question?'
+    ];
+    var sentenceGroup = expected.join(' ');
+    var result = markov.sentencize(sentenceGroup);
+    assert.isArray(result);
+    assert.lengthOf(result, expected.length - 1, 'should have returned ' + expected.length + ' sentences');
+    assert.equal(result[0], expected[0], 'should have returned sentences');
+    assert.equal(result[1], expected[2], 'should have returned sentences');
+    done();
+  });
+});
+describe('#tokenizeSentence', function() {
+  it('should return array of tokens', function(done) {
+    var sentence = 'This is a sentence.';
+    var expected = ['This', 'is', 'a', 'sentence', '.'];
+    var result = markov.tokenizeSentence(sentence);
+    assert.isArray(result);
+    assert.lengthOf(result, expected.length, 'should have returned ' + expected.length + ' tokens');
+    assert.deepEqual(result, expected, 'should have returned tokens');
     done();
   });
 });
