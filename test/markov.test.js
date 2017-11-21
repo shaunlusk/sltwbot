ed4fd000fbc17efbc4bd880f8211afd221af53d5
Shaun Lusk <shaun.lusk@gmail.com>
2017-11-20 21:54:27 -0600

    Make Key tests

--- test/markov.test.js
+++ test/markov.test.js
@@ -117,39 +117,189 @@ describe('#tokenizeSentence', function() {
     done();
   });
 });
-describe('#wordCachify', function() {
-  it('should setup word cache', function(done) {
-    var sentence = 'This is a sentence.';
-    var wordCache = markov.initializeWordCache();
-    var expected = markov.initializeWordCache();
-    expected.This = {'is':1};
-    expected.is = {'a':1};
-    expected.a = {'sentence':1};
-    expected.sentence = {'END':{'.':1}};
+describe('#getEntry', function() {
+  it('retrieves the current entry of the hashmap', function(done) {
+    var wordCache = {
+      'test' : {'whatever':1}
+    };
+    var token = 'test';
 
-    markov.wordCachify(wordCache, sentence);
+    var result = markov.getEntry(wordCache, token);
 
-    assert.isObject(wordCache);
-    assert.deepEqual(wordCache, expected, 'should setup word cache');
+    assert.deepEqual(result, wordCache.test);
     done();
   });
-  it('should setup word cache', function(done) {
-    var sentence = 'This is a sentence.';
-    var sentence2 = 'This is a slightly different sentence.';
-    var wordCache = markov.initializeWordCache();
-    var expected = markov.initializeWordCache();
-    expected.This = {'is':2};
-    expected.is = {'a':2};
-    expected.a = {'sentence':1, 'slightly':1};
-    expected.sentence = {'END':{'.':1}};
-    expected.slightly = {'different':1};
-    expected.different =  {'sentence': 1};
+  it('creates an entry in the hash map', function(done) {
+    var wordCache = {};
+    var token = 'test';
 
-    markov.wordCachify(wordCache, sentence);
-    markov.wordCachify(wordCache, sentence2);
+    var result = markov.getEntry(wordCache, token);
 
-    assert.isObject(wordCache);
-    assert.deepEqual(wordCache, expected, 'should setup word cache');
+    assert.isObject(result);
+    assert.isObject(wordCache[token]);
     done();
   });
 });
+describe('#makeKey', function() {
+  it('make first order key from index 0', function(done) {
+    var tokens = ['this', 'is'];
+    var targetIndex = 0;
+    var order = 1;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.equal(result, 'START');
+    done();
+  });
+  it('make first order key from index 1', function(done) {
+    var tokens = ['this', 'is'];
+    var targetIndex = 1;
+    var order = 1;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.equal(result, 'this');
+    done();
+  });
+  it('make first order key from index 2', function(done) {
+    var tokens = ['this', 'is', 'a'];
+    var targetIndex = 2;
+    var order = 1;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.equal(result, 'is');
+    done();
+  });
+  it('make second order key from index 0', function(done) {
+    var tokens = ['this', 'is', 'a'];
+    var targetIndex = 0;
+    var order = 2;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, 'START');
+    done();
+  });
+  it('make second order key from index 1', function(done) {
+    var tokens = ['this', 'is', 'a'];
+    var targetIndex = 1;
+    var order = 2;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, 'START' + ' ' + tokens[0]);
+    done();
+  });
+  it('make second order key from index 2', function(done) {
+    var tokens = ['this', 'is', 'a'];
+    var targetIndex = 2;
+    var order = 2;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, tokens[0] + ' ' + tokens[1]);
+    done();
+  });
+  it('make second order key from index 3', function(done) {
+    var tokens = ['this', 'is', 'a', 'sentence'];
+    var targetIndex = 3;
+    var order = 2;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, tokens[1] + ' ' + tokens[2]);
+    done();
+  });
+  it('make third order key from index 0', function(done) {
+    var tokens = ['this', 'is', 'a'];
+    var targetIndex = 0;
+    var order = 3;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, 'START');
+    done();
+  });
+  it('make third order key from index 1', function(done) {
+    var tokens = ['this', 'is', 'a'];
+    var targetIndex = 1;
+    var order = 3;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, 'START' + ' ' + tokens[0]);
+    done();
+  });
+  it('make third order key from index 2', function(done) {
+    var tokens = ['this', 'is', 'a'];
+    var targetIndex = 2;
+    var order = 3;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, 'START' + ' ' + tokens[0] + ' ' + tokens[1]);
+    done();
+  });
+  it('make third order key from index 3', function(done) {
+    var tokens = ['this', 'is', 'a', 'sentence'];
+    var targetIndex = 3;
+    var order = 3;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, tokens[0] + ' ' + tokens[1] + ' ' + tokens[2]);
+    done();
+  });
+  it('make third order key from index 4', function(done) {
+    var tokens = ['this', 'is', 'a', 'sentence', '.'];
+    var targetIndex = 4;
+    var order = 3;
+
+    var result = markov.makeKey(tokens, targetIndex, order);
+
+    assert.deepEqual(result, tokens[1] + ' ' + tokens[2] + ' ' + tokens[3]);
+    done();
+  });
+});
+
+// describe('#wordCachify', function() {
+//   describe('1st Order', function() {
+//     it('should setup word cache', function(done) {
+//       var sentence = 'This is a sentence.';
+//       var wordCache = markov.initializeWordCache();
+//       var expected = markov.initializeWordCache();
+//       expected.START.this = 1;
+//       expected.this = {'is':1};
+//       expected.is = {'a':1};
+//       expected.a = {'sentence':1};
+//       expected.sentence = {'END':{'.':1}};
+//
+//       markov.wordCachify(wordCache, sentence, 1);
+//
+//       assert.isObject(wordCache);
+//       assert.deepEqual(wordCache, expected, 'should setup word cache');
+//       done();
+//     });
+//     it('should setup word cache', function(done) {
+//       var sentence = 'This is a sentence.';
+//       var sentence2 = 'This is a slightly different sentence.';
+//       var wordCache = markov.initializeWordCache();
+//       var expected = markov.initializeWordCache();
+//       expected.START.this = 2;
+//       expected.this = {'is':2};
+//       expected.is = {'a':2};
+//       expected.a = {'sentence':1, 'slightly':1};
+//       expected.sentence = {'END':{'.':2}};
+//       expected.slightly = {'different':1};
+//       expected.different =  {'sentence': 1};
+//
+//       markov.wordCachify(wordCache, sentence, 1);
+//       markov.wordCachify(wordCache, sentence2, 1);
+//
+//       assert.isObject(wordCache);
+//       assert.deepEqual(wordCache, expected, 'should setup word cache');
+//       done();
+//     });
+//   });
+// });
