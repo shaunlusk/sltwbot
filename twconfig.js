c6c142fdf8153b4734c6843e78bfcc7804147d7b
Shaun Lusk <shaun.lusk@gmail.com>
2017-11-26 17:43:23 -0600

    Removing twitter secrets.  Will regenerate.

--- twconfig.js
+++ twconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'YadEtZakSS6NWuw8I12cydQgH',
-  consumer_secret:      'PPjPVbs7sGIPpGDhAaVt485qV1p5KlL4V8dHf6XyHIgFjPgJLj',
-  access_token:         '929531918835691521-mbgru9XnGcQIgtHRDFuyOc3ShFMM4JJ',
-  access_token_secret:  'Wo5fOyP8MP8KfFfjJoNHB2hk06UrTIIo6g4myZOE0a5c9'
+  consumer_key:         'your_consumer_key',
+  consumer_secret:      'your_consumer_secret',
+  access_token:         'your_access_token',
+  access_token_secret:  'your_access_token_secret'
 };
